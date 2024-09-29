d145338ec40d0204c44643d484bdae4839f2f3fc
pradeepks <kumarks.pradeep@gmail.com>
2024-09-30 00:20:17 +0100

    Added missing files

--- database/01-initial-db.sql
+++ database/01-initial-db.sql
@@ -2,7 +2,8 @@ use definitions;
 
 CREATE TABLE IF NOT EXISTS `dashboards` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `createdAt` TIMESTAMP NOT NULL DEFAULT NOW(),
-  `updatedAt` TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+  `created_at` TIMESTAMP NOT NULL DEFAULT NOW(),
+  `updated_at` TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+  `title` TEXT,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
