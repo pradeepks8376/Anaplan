0949d005cbdaafdc6fc2d229a9ae34277aad06b2
pradeepks <kumarks.pradeep@gmail.com>
2024-09-30 00:39:10 +0100

    Loggers addded

--- bring-your-own-interview/src/main/java/com/anaplan/assignment/controller/DashboardController.java
+++ bring-your-own-interview/src/main/java/com/anaplan/assignment/controller/DashboardController.java
@@ -4,7 +4,9 @@ import com.anaplan.assignment.exception.DashboardNotFoundException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,9 +15,9 @@ import com.anaplan.assignment.model.Dashboard;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/anaplan")
-
 public class DashboardController {
 
     @Autowired
@@ -30,13 +32,14 @@ public class DashboardController {
             @ApiResponse(responseCode = "200", description = "Successfully retrieved list"),
             @ApiResponse(responseCode = "500", description = "Internal server error")
     })
-    @GetMapping("/dashboards")
+    @GetMapping(value = "/dashboards", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Dashboard>>  getDashboards() {
+        log.info("DashboardController getDashboards:");
         List<Dashboard> dashboards = dashboardService.getAllDashboards();
         if (dashboards == null || dashboards.isEmpty()) {
             throw new DashboardNotFoundException("No dashboard found: ");
         }
         return ResponseEntity.ok(dashboards);
     }
-
+    
 }
--- bring-your-own-interview/src/main/java/com/anaplan/assignment/service/DashboardService.java
+++ bring-your-own-interview/src/main/java/com/anaplan/assignment/service/DashboardService.java
@@ -2,10 +2,12 @@ package com.anaplan.assignment.service;
 
 import com.anaplan.assignment.model.Dashboard;
 import com.anaplan.assignment.repo.DashboardRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class DashboardService {
 
@@ -16,6 +18,7 @@ public class DashboardService {
     }
 
     public List<Dashboard>  getAllDashboards() {
+        log.info("In DashboardService - getAllDashboards");
         return dashboardRepository.findAll();
     }
 }
